18bd5bc69be443b345d994d848b9dfcaaffcffe8
markchawkins53 <markchawkins53@gmail.com>
2018-05-17 21:41:31 -0400

    pls
    work

---
+++ Source/Hyper_Hand/Hyper_Hand.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Hyper_Hand.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hyper_Hand, "Hyper_Hand" );
